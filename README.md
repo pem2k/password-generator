d92147b178126f50e60a221cb0be2ce4f065f619
Parker McKillop <mckilpar000@hotmail.com>
2022-06-27 12:28:31 -0700

    included 0 in the number character set, updated readMe

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Password Generator Starter Code
+# password-generator
+
+## Description
+
+I built this project to allow users to generate secure passwords.
+
+I learned a lot in this project about manipulating strings and arrays, input validation,  as well as psuedo randomization.
+
+If I had to code this again, I would insert an extra check on the generated password to confirm at least one character of each of the chosen sets are included. That being said, based on the probability of a requested character set not showing up at the smallest password length, I believe this was safe to leave out.
+
+## Usage
+
+https://pem2k.github.io/password-generator
+
+    ```md
+    ![generated password](assets/images/screenshot.png)
+    ```
+
+## License
+
+MIT License
+
+Copyright (c) [2022] [Parker McKillop]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,25 @@
 // Assignment code here
 function generatePassword(){
-//object that stores password parameters
-  //if a key value of param is true, push a specified array to a new array as an element
 
-    let length = prompt("Please input desired password length, length must be 8-128 characters");
+  let length = prompt("Please input desired password length, length must be 8-128 characters");
       
-    //validates password length and recursively reprompts if invalid
-    lengthVal = function() {
-      if ( 128 < length || length < 8  || length == undefined || isNaN(length) === true){
-        length = prompt("Please input desired password length, length must be 8-128 characters"); 
-        lengthVal();
-      } 
-    }
-    lengthVal();
+  //validates password length and input character type - recursively reprompts if invalid
+  lengthVal = function() {
+    if ( 128 < length || length < 8  || length == undefined || isNaN(length) === true){
+      length = prompt("Please input desired password length, length must be 8-128 characters"); 
+      lengthVal();
+    };
+  };
+  lengthVal();
         
-    let lower = confirm("Click \"ok\" if you'd like your password to contain lowercase letters, and \"cancel\" if you do not." );
-    let upper = confirm("Click \"ok\" if you'd like your password to contain uppercase letters, and \"cancel\" if you do not.");
-    let number = confirm("Click \"ok\" if you'd like your password to contain numeric characters, and \"cancel\" if you do not.");
-    let special = confirm("Click \"ok\" if you'd like your password to contain special characters, and \"cancel\" if you do not.");
+  let lower = confirm("Click \"ok\" if you'd like your password to contain lowercase letters, and \"cancel\" if you do not." );
+  let upper = confirm("Click \"ok\" if you'd like your password to contain uppercase letters, and \"cancel\" if you do not.");
+  let number = confirm("Click \"ok\" if you'd like your password to contain numeric characters, and \"cancel\" if you do not.");
+  let special = confirm("Click \"ok\" if you'd like your password to contain special characters, and \"cancel\" if you do not.");
 
     
   //validates password parameters and recursively re-prompts if choices are invalid
-  let paramVal = function (){
+  let paramVal = function () {
     if (lower === false && upper === false && number === false && special === false) {
       alert("You must choose valid password parameters");
       lower = confirm("Click \"ok\" if you'd like your password to contain lowercase letters, and \"cancel\" if you do not." );
@@ -35,9 +33,9 @@ function generatePassword(){
   paramVal();
 
   //available character sets
-    let arrLow = "abcdefghijklmnopqrstuvwxyz".split("");
+    let arrLow = "abcdefghijklmnopqrstuvwxyz".split(""); 
     let arrUp = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
-    let arrNum = "123456789".split("");
+    let arrNum = "0123456789".split("");
     let arrSpec= "!\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~".split("");
   
   //creates an array where character set arrays will be pushed as elements, this will be iterated over to generate the password
@@ -63,13 +61,16 @@ function generatePassword(){
     chosenSet.push(arrSpec);
   };
 
+  //chooses 1 of the character type arrays, then based on chosen array, a random character is chosen and pushed to the genRes array
   for(let i=0; i < length; i++) {
    let randArr =  chosenSet[Math.floor(Math.random()*chosenSet.length)];
    let randChar = randArr[Math.floor(Math.random()*randArr.length)];
-   genRes.push(randChar)
+   genRes.push(randChar);
   };
 
-   return genRes.join("");
+  //returns the genRes array joined into a string
+  return genRes.join("");
+
 };
 
 // Get references to the #generate element
